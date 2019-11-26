324b5b3cf4b70da9655cbca0fc9aaec06d289f31
Strahinja <djuricastrahinja@gmail.com>
2019-11-26 15:00:07 +0100

    Add select method

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,15 +3,15 @@ class Anagram
 
   def anagrams
     dictionary = read("anagrams-wordlist.txt")
-    dictionary.group_by{|word| word.downcase.delete("^a-z").each_char.sort}.values
+    all_anagrams = dictionary.group_by{|word| word.downcase.delete("^a-z").each_char.sort}.values
+    result = all_anagrams.select { |element| element.length > 1 }
+    result
   end
 
   def print_anagrams
     anagrams = anagrams()
     anagrams.each do |line|
-      if line.length > 1
-        print "#{line.each {|word| word.delete!("\n")}.join(", ")}\n"
-      end
+      print "#{line.each {|word| word.delete!("\n")}.join(", ")}\n"
     end
   end
 
