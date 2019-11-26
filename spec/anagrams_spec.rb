01ad73b15e757beec472164db4bc3acb88a40ba2
Strahinja <djuricastrahinja@gmail.com>
2019-11-26 22:10:57 +0100

    Remove dummy file from testing

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,24 +3,32 @@ require "anagrams"
 describe Anagram do
   describe ".anagrams" do
     sample = Anagram.new
-    all_anagrams = sample.anagrams("simple-anagrams.txt")
+    all_anagrams = sample.anagrams(["cile","mile","pile","LEMI","drakos","drako's"])
     it 'should get all anagrams' do
       expect(all_anagrams.count).to be 2
     end
 
     it 'should include test_arr' do
-      test_arr = ["mile\n", "LEMI\n"]
+      test_arr = ["mile", "LEMI"]
       expect(all_anagrams).to include(test_arr)
     end
 
     it 'should be case insensitive' do
-      test_arr = ["mile\n", "LEMI\n"]
+      test_arr = ["mile", "LEMI"]
       expect(all_anagrams).to include(test_arr)
     end
 
     it 'should be non-letter insensitive' do
-      test_arr = ["drakos\n", "drako's\n"]
+      test_arr = ["drakos", "drako's"]
       expect(all_anagrams).to include(test_arr)
+    end
+  end
+
+  describe ".read" do
+    sample = Anagram.new
+    file = sample.read("simple-anagrams.txt")
+    it 'should get all anagrams from file' do
+      expect(sample.anagrams(file).count).to be 2
     end
   end
 end
