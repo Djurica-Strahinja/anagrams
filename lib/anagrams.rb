484d8b37490ceeda497fa7f5fd18b90b87a2adad
Strahinja <djuricastrahinja@gmail.com>
2019-11-26 22:04:48 +0100

    Remove read method from anagrams method

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,7 +1,6 @@
 class Anagram
-  def anagrams(file_name)
-    dictionary = read(file_name)
-    all_anagrams = dictionary.group_by {|word| word.downcase.delete("^a-z").each_char.sort}.values
+  def anagrams(word_list)
+    all_anagrams = word_list.group_by {|word| word.downcase.delete("^a-z").each_char.sort}.values
     result = all_anagrams.select { |element| element.length > 1 }
     result
   end
@@ -13,7 +12,6 @@ class Anagram
     end
   end
 
-  private
   def read(file_name)
     Dir.chdir(File.dirname(__FILE__))
     File.open(file_name) do |file|
