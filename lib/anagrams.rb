923c7e00ed8b9ddaae6151eb7c24876c6d1f4a0a
Strahinja <djuricastrahinja@gmail.com>
2019-11-26 15:10:35 +0100

    Fix identations

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,9 +1,7 @@
 class Anagram
-
-
   def anagrams
     dictionary = read("anagrams-wordlist.txt")
-    all_anagrams = dictionary.group_by{|word| word.downcase.delete("^a-z").each_char.sort}.values
+    all_anagrams = dictionary.group_by {|word| word.downcase.delete("^a-z").each_char.sort}.values
     result = all_anagrams.select { |element| element.length > 1 }
     result
   end
